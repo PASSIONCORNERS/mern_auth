5f7c711e30ff3bd4ed8a9ac18b76d918f435d655
Phong Truong <phongtruong@Phongs-MacBook-Pro.local>
2021-08-12 22:59:03 +0700

    lesson-6

---
+++ client/src/components/Input/Input.js
@@ -0,0 +1,34 @@
+import "./input.css";
+
+const Input = ({
+  icon,
+  handleClick,
+  type,
+  name,
+  handleChange,
+  defaultValue,
+  disabled,
+  text,
+}) => {
+  return (
+    <div className="input">
+      <label>
+        <div className="input_icon" onClick={handleClick}>
+          {icon}
+        </div>
+        <input
+          type={type}
+          name={name}
+          onChange={handleChange}
+          defaultValue={defaultValue}
+          disalbed={disabled}
+          placeholder="&nbsp;"
+          autoComplete="off"
+        />
+        <span>{text}</span>
+      </label>
+    </div>
+  );
+};
+
+export default Input;
--- client/src/components/Login/Login.js
+++ client/src/components/Login/Login.js
@@ -1,10 +1,21 @@
+import { FcGoogle } from "react-icons/fc";
+import { MdVisibility } from "react-icons/md";
+import { MdVisibilityOff } from "react-icons/md";
+import Input from "../Input/Input";
 import "./login.css";
 
 const Login = () => {
   return (
-    <div className="login">
-      <p>Login</p>
-    </div>
+    <form className="login">
+      <Input type="email" text="Email" />
+      <Input type="password" text="Password" />
+      <div className="login_btn">
+        <button>login</button>
+        <button className="btn-alt">
+          sign in <FcGoogle />
+        </button>
+      </div>
+    </form>
   );
 };
 
