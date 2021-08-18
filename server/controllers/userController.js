d063843d6385bafbc86d177d4a0edf0ac40bf012
Phong Truong <phongtruong@Phongs-MacBook-Pro.local>
2021-08-19 00:39:21 +0700

    lesson-17

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -2,6 +2,7 @@ const sendMail = require("../helpers/sendMail");
 const createToken = require("../helpers/createToken");
 const validateEmail = require("../helpers/validateEmail");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 const User = require("../models/userModel");
 const { activation } = require("../helpers/createToken");
 
@@ -29,7 +30,7 @@ const userController = {
           .json({ msg: "This email is already registered in our system." });
 
       // check password
-      if (password.lenght < 6)
+      if (password.length < 6)
         return res
           .status(400)
           .json({ msg: "Password must be at least 6 characters." });
@@ -48,6 +49,38 @@ const userController = {
 
       // registration success
       res.status(200).json({ msg: "Welcome! Please check your email." });
+    } catch (err) {
+      res.status(500).json({ msg: err.message });
+    }
+  },
+  activate: async (req, res) => {
+    try {
+      // get token
+      const { activation_token } = req.body;
+
+      // verify token
+      const user = jwt.verify(activation_token, process.env.ACTIVATION_TOKEN);
+      const { name, email, password } = user;
+
+      // check user
+      const check = await User.findOne({ email });
+      if (check)
+        return res
+          .status(400)
+          .json({ msg: "This email is already registered." });
+
+      // add user
+      const newUser = new User({
+        name,
+        email,
+        password,
+      });
+      await newUser.save();
+
+      // activation success
+      res
+        .status(200)
+        .json({ msg: "Your account has been activated, you can now sign in." });
     } catch (err) {
       res.status(500).json({ msg: err.message });
     }
--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -3,5 +3,6 @@ const route = Router();
 const userController = require("../controllers/userController");
 
 route.post("/api/auth/register", userController.register);
+route.post("/api/auth/activation", userController.activate);
 
 module.exports = route;
