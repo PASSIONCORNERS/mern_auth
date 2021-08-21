30d2d989ab26891aa28a7afa9d1af55152c70d2a
Phong Truong <phongtruong@Phongs-MacBook-Pro.local>
2021-08-21 21:58:26 +0700

    lesson-19 update

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -5,13 +5,7 @@ const auth = require("../middlewares/auth");
 
 route.post("/api/auth/register", userController.register);
 route.post("/api/auth/activation", userController.activate);
-route.post("/api/auth/signin", userController.signin);
+route.post("/api/auth/signing", userController.signing);
 route.post("/api/auth/access", userController.access);
-route.post("/api/auth/forgot_pass", userController.forgot);
-route.post("/api/auth/reset_pass", auth, userController.reset);
-route.get("/api/auth/user", auth, userController.info);
-route.patch("/api/auth/user_update", auth, userController.update);
-route.get("/api/auth/signout", userController.signout);
-route.post("/api/auth/google_signin", userController.google);
 
 module.exports = route;
