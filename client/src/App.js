3fa15d83cbd68cf99d329048c2ae2ce9f8e0ac5a
Phong Truong <phongtruong@Phongs-MacBook-Pro.local>
2021-08-27 14:56:23 +0700

    lesson-26 update

--- client/src/App.js
+++ client/src/App.js
@@ -4,10 +4,37 @@ import AuthLayout from "./Layouts/AuthLayout/AuthLayout";
 import ProfileLayout from "./Layouts/ProfileLayout/ProfileLayout";
 import ResetLayout from "./Layouts/ResetLayout/ResetLayout";
 import { AuthContext } from "./context/AuthContext";
-import { useContext } from "react";
+import { useContext, useEffect } from "react";
+import axios from "axios";
 
 function App() {
-  const { isLoggedIn } = useContext(AuthContext);
+  const { dispatch, token, isLoggedIn } = useContext(AuthContext);
+
+  // get ac token
+  useEffect(() => {
+    const _appSignging = localStorage.getItem("_appSignging");
+    if (_appSignging) {
+      const getToken = async () => {
+        const res = await axios.post("/api/auth/access", null);
+        dispatch({ type: "GET_TOKEN", payload: res.data.ac_token });
+      };
+      getToken();
+    }
+  }, [dispatch, isLoggedIn]);
+
+  // get user data
+  useEffect(() => {
+    if (token) {
+      const getUser = async () => {
+        dispatch({ type: "SIGNING" });
+        const res = await axios.get("/api/auth/user", {
+          headers: { Authorization: token },
+        });
+        dispatch({ type: "GET_USER", payload: res.data });
+      };
+      getUser();
+    }
+  }, [dispatch, token]);
 
   return (
     <Router>
--- client/src/components/Login/Login.js
+++ client/src/components/Login/Login.js
@@ -1,33 +1,86 @@
-import { useState } from "react";
+import { useContext, useState } from "react";
 import { FcGoogle } from "react-icons/fc";
 import { MdVisibility } from "react-icons/md";
 import { MdVisibilityOff } from "react-icons/md";
+import { isEmpty, isEmail } from "../helper/validate";
+import axios from "axios";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import Input from "../Input/Input";
 import "./login.css";
+import { AuthContext } from "../../context/AuthContext";
+
+const initialState = {
+  name: "",
+  password: "",
+};
 
 const Login = () => {
   const [visible, setVisible] = useState(false);
+  const [data, setData] = useState(initialState);
+  const { email, password } = data;
+  const { dispatch } = useContext(AuthContext);
+
+  const handleChange = (e) => {
+    setData({ ...data, [e.target.name]: e.target.value });
+  };
 
   const handleClick = () => {
     setVisible(!visible);
   };
 
+  const login = async (e) => {
+    e.preventDefault();
+    // check fields
+    if (isEmpty(email) || isEmpty(password))
+      return toast("Please fill in all fields.", {
+        className: "toast-failed",
+        bodyClassName: "toast-failed",
+      });
+    // check email
+    if (!isEmail(email))
+      return toast("Please enter a valid email addresss.", {
+        className: "toast-failed",
+        bodyClassName: "toast-failed",
+      });
+    try {
+      await axios.post("/api/auth/signing", { email, password });
+      localStorage.setItem("_appSignging", true);
+      dispatch({ type: "SIGNING" });
+    } catch (err) {
+      toast(err.response.data.msg, {
+        className: "toast-failed",
+        bodyClassName: "toast-failed",
+      });
+    }
+  };
+
   return (
-    <form className="login">
-      <Input type="email" text="Email" />
-      <Input
-        type={visible ? "text" : "password"}
-        icon={visible ? <MdVisibility /> : <MdVisibilityOff />}
-        text="Password"
-        handleClick={handleClick}
-      />
-      <div className="login_btn">
-        <button>login</button>
-        <button className="btn-alt">
-          sign in <FcGoogle />
-        </button>
-      </div>
-    </form>
+    <>
+      <ToastContainer />
+      <form className="login" onSubmit={login}>
+        <Input
+          type="email"
+          text="Email"
+          name="email"
+          handleChange={handleChange}
+        />
+        <Input
+          name="password"
+          type={visible ? "text" : "password"}
+          icon={visible ? <MdVisibility /> : <MdVisibilityOff />}
+          text="Password"
+          handleClick={handleClick}
+          handleChange={handleChange}
+        />
+        <div className="login_btn">
+          <button type="submit">login</button>
+          <button className="btn-alt">
+            sign in <FcGoogle />
+          </button>
+        </div>
+      </form>
+    </>
   );
 };
 
